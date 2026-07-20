b89d2da78065a0d68bb7ae6478e586c887427b85
hamuzon <github@hamusata.f5.si>
2026-07-20 13:10:55 +0900

    SpeedInsightsコンポーネントを削除し、Appコンポーネントを簡素化

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,3 @@
-import { SpeedInsights } from '@vercel/speed-insights/next';
-
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <SpeedInsights />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
