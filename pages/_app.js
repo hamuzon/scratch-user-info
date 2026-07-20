ac07f8bba69066c1ceaa16cc8ed128f96dcb7d61
hamuzon <github@hamusata.f5.si>
2026-07-20 14:37:38 +0900

    条件付きでSpeedInsightsコンポーネントを表示するように変更

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <SpeedInsights />
+      {!isGitHubPages && <SpeedInsights />}
     </>
   );
 }
